9e3d9e859f88398bf74b393ed7e8a3b64df7d98c
rctrollex <rctrollex@gmail.com>
2025-05-13 18:16:57 +0200

    Added Hero Section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <footer className="bg-gray-800 text-gray-500 pt-24 p-4">
+        <footer className="bg-gray-800 text-gray-500 p-4">
             <div className="container text-center flex-row mx-auto px-4">
                 <p className="text-sm">Copyright &copy; 2025 Bookmark Haven. Created by Ryan Chigwengwe.</p>
                 <div className="space-x-4 justify-center flex mt-4">
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,25 @@ const Home = () => {
     return (
         <dv className="relative overflow-hidden">
             <Navbar />
+            <section className="pt-24 pb-16 bg-gradient-to-br from-blue-100 to-indigo-200">
+                <div className="container mx-auto px-4 text-center">
+                    <h2 className="text-5xl md:text-6xl sm:text-2xl font-bold mb-4 text-gray-900 leading-tight">Your Links,
+                        <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-600 to-indigo-600">Securely Organized</span>
+                    </h2>
+                    <p className="text-lg md:text-xl text-gray-600 max-w-2xl mb-8 mx-auto">
+                        Bookmark Haven lets you save, categorize,
+                        and access your favorite links with secure authentication and cloud storage,
+                        powered by Appwrite.
+                    </p>
+
+                    <div className="flex justify-center  gap-4">
+                        <a href="#signUp" className="bg-gradient-to-r from-blue-600 to-indigo-600 px-4 py-4 rounded-lg text-white shadow-lg inline-block hover:from-blue-700 hover:to-indigo-700 transform hover:scale-105 transition-all duration-75">Get Started</a>
+                        <a href="login" className="bg-gray-50 px-8 py-4 rounded-lg shadow-lg inline-block hover:bg-gray-200 text-indigo-600 transform hover:scale-105 transition-all duration-75">Login</a>
+
+                    </div>
+                </div>
+
+            </section>
             <Footer />
         </dv>
     )
