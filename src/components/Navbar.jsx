a6af730840cb00b8660505b68bf10a071951c06a
rctrollex <rctrollex@gmail.com>
2025-05-13 21:31:27 +0200

    Added Features Section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
     return (
       <nav className="bg-white fixed w-full shadow-sm z-10">
           <div className="container mx-auto justify-between items-center py-4 px-4 flex">
-              <h1 className="text-2xl sm:text-lg font-bold bg-clip-text text-transparent bg-gradient-to-r from-blue-600 to-indigo-600">Bookmark Haven</h1>
+              <h1 className="sm:text-lg lg:text-3xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-blue-600 to-indigo-600">Bookmark Haven</h1>
               <div className="space-x-2 flex-row">
                   <a href="#features" className="text-md text-gray-600 sm:text-sm hover:text-indigo-600 transition">Features</a>
                   <a href="#login" className="text-md text-gray-600 sm:text-sm hover:text-indigo-600 transition">Login</a>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,11 +4,11 @@ import Footer from "../components/Footer.jsx";
 
 const Home = () => {
     return (
-        <dv className="relative overflow-hidden">
+        <div className="relative overflow-hidden">
             <Navbar />
             <section className="pt-24 pb-16 bg-gradient-to-br from-blue-100 to-indigo-200">
                 <div className="container mx-auto px-4 text-center">
-                    <h2 className="text-5xl md:text-6xl sm:text-2xl font-bold mb-4 text-gray-900 leading-tight">Your Links,
+                    <h2 className="text-5xl md:text-6xl sm:text-4xl font-bold mb-4 text-gray-900 leading-tight">Your Links,
                         <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-600 to-indigo-600">Securely Organized</span>
                     </h2>
                     <p className="text-lg md:text-xl text-gray-600 max-w-2xl mb-8 mx-auto">
@@ -20,13 +20,49 @@ const Home = () => {
                     <div className="flex justify-center  gap-4">
                         <a href="#signUp" className="bg-gradient-to-r from-blue-600 to-indigo-600 px-4 py-4 rounded-lg text-white shadow-lg inline-block hover:from-blue-700 hover:to-indigo-700 transform hover:scale-105 transition-all duration-75">Get Started</a>
                         <a href="login" className="bg-gray-50 px-8 py-4 rounded-lg shadow-lg inline-block hover:bg-gray-200 text-indigo-600 transform hover:scale-105 transition-all duration-75">Login</a>
-
                     </div>
                 </div>
-
             </section>
-            <Footer />
-        </dv>
+
+            <section id="features" className="bg-white py-10">
+                <div className="container mx-auto px-4">
+                    <h3 className="text-3xl font-bold text-gray-900 text-center mb-12">Powerful Features for Your Bookmarks</h3>
+                    <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 py-3">
+                        <div className="bg-gray-50 p-6 rounded-2xl shadow-lg transform hover:translate-y-1 transition-all duration-300">
+                            <div className="flex justify-start">
+                                <img src ="/src/assets/addIcon.svg" className="w-12 h-12 mb-4" alt="manage bookmarks icon" />
+                            </div>
+                            <h4 className="text-gray-900 font-bold text-lg ">Manage Bookmarks</h4>
+                            <p className="text-gray-500">Easily add, edit, or delete bookmarks with a clean intuitive interface</p>
+                        </div>
+
+                        <div className="bg-gray-50 p-6 rounded-2xl shadow-lg transform hover:translate-y-1 transition-all duration-300">
+                            <div className="flex justify-start">
+                                <img src="/src/assets/categorizeLogo.svg" className="w-12 h-12 mb-4" alt="Categorization logo"/>
+                            </div>
+                            <h4 className="text-gray-900 text-lg font-bold">Smart Categorization</h4>
+                            <p className="text-gray-500">Organize bookmarks into categories like work or Learning for quick access.</p>
+                        </div>
+
+                        <div className="bg-gray-50 p-6 rounded-2xl shadow-lg transform hover:translate-y-1 transition-all duration-300">
+                            <div className="flex justify-start">
+                                <img src="/src/assets/secureLogo.svg" className="w-12 h-12 mb-4" alt="Secure Authenication Logo"/>
+                            </div>
+                            <h4 className="text-gray-900 font-bold text-lg">Secure Authentication</h4>
+                            <p className="text-gray-500">Log in securely with email/password or OAuth, powered by Appwrite's robust auth system.</p>
+                        </div>
+
+                        <div className="bg-gray-50 p-6 rounded-2xl shadow-lg transform hover:translate-y-1 transition-all duration-300">
+                            <div className="flex justify-start">
+                                <img src="/src/assets/cloudLogo.svg" className="w-12 h-12 mb-4" alt="Cloud Storage Logo"/>
+                            </div>
+                            <h4 className="text-gray-900 text-lg font-bold">Cloud Storage</h4>
+                            <p className="text-gray-500">Store bookmarks securely in the cloud with the Appwrite scalable database.</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
     )
 }
 export default Home
