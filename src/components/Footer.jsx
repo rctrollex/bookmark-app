24631ab17884ecd667d69f92dda0fa5676011356
rctrollex <rctrollex@gmail.com>
2025-05-13 17:31:51 +0200

    Added footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <footer className="bg-gray-800 text-gray-500 pt-24 p-4">
+            <div className="container text-center flex-row mx-auto px-4">
+                <p className="text-sm">Copyright &copy; 2025 Bookmark Haven. Created by Ryan Chigwengwe.</p>
+                <div className="space-x-4 justify-center flex mt-4">
+                    <a href="#" className="text-sm hover:text-indigo-600 transition">Privacy Policy</a>
+                    <a href="#" className="text-sm hover:text-indigo-600 transition">Terms of Service</a>
+                </div>
+            </div>
+        </footer>
     )
 }
 export default Footer
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from "../components/Navbar.jsx";
+import Footer from "../components/Footer.jsx";
 
 const Home = () => {
     return (
         <dv className="relative overflow-hidden">
             <Navbar />
+            <Footer />
         </dv>
     )
 }
